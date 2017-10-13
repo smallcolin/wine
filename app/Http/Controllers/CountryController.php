632fc2bc2eff17c89b20ae1710c7837d25f6914f
smallcolin <colinsweeney2000@gmail.com>
2017-10-13 21:15:13 +0200

    added comments to all code

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -11,47 +11,59 @@ class CountryController extends Controller
 {
   public function index()
   {
+    // Get all countries from table
     $countries = Country::all();
-
+    // Direct to show all countries page
     return view('admin.countries.index')->with('countries', $countries);
   }
 
   public function create()
   {
+    // Show form to create a country
     return view('admin.countries.create');
   }
 
   public function store()
   {
+    // Validate form data
     $this->validate(request(), [
       'name' => 'required|string'
     ]);
 
+    // Prepare data and save to table
     $country = new Country;
     $country->name = request()->name;
     $country->save();
 
+    // Display a message
     Session::flash('success', 'Country was successfully created!');
 
+    // redirect to show all countries
     return redirect('/countries');
   }
 
   public function edit(Country $country)
   {
+    // Show the form to edit a country
     return view('admin.countries.edit')->with('country', $country);
   }
 
   public function update($id)
   {
+    // Validate form data
     $this->validate(request(), [
       'name' => 'required|string|max:40'
     ]);
+
+    // Prepare data and save to table
     $country = Country::findOrFail($id);
     $country->name = request()->name;
     $country->save();
 
+    // Display a message
     Session::flash('success', 'Country was updated');
 
+    // Redirect admin back to show all countries
     return redirect()->route('country.show');
   }
 
@@ -63,13 +75,13 @@ class CountryController extends Controller
     // SQL search to find any use of id in Wine table
     $winesWithCountry = Wine::where('country_id', $id)->get();
 
-    // Disable delete if any use is found
+    // Disable delete if country is associated with a wine
     if (count($winesWithCountry) > 0) {
       Session::flash('error', 'You can\'t delete a country that is attached to a wine!');
       return redirect()->back();
     }
 
-    //
+    // Delete database entry
     $country->delete();
 
     // Display a message

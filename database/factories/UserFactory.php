d5eb72a09a8f5ecc301447b81af5d159ba1bcda5
smallcolin <colinsweeney2000@gmail.com>
2017-10-15 22:03:15 +0200

    changed datbase seeding so as not to create zero values

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,13 +31,13 @@ $factory->define(App\Wine::class, function (Faker $faker) {
     static $password;
 
     return [
-      'country_id' => $faker->randomDigit,
-      'customer_id' => $faker->randomDigit,
+      'country_id' => $faker->numberBetween($min=1, $max=10),
+      'customer_id' => $faker->numberBetween($min=1, $max=3),
       'name' => $faker->unique()->text($maxNbChars = 10),
       'description' => $faker->text($maxNbChars = 100),
-      'price' => $faker->randomDigit,
-      'stock' => $faker->randomDigit,
-      'approved' => $faker->randomDigit,
+      'price' => $faker->numberBetween($min=1, $max=10),
+      'stock' => $faker->numberBetween($min=0, $max=50),
+      'approved' => $faker->numberBetween($min=0, $max=1),
       'image_url' => $faker->imageUrl($width = 640, $height = 480)
     ];
 });
@@ -56,8 +56,8 @@ $factory->define(App\Comment::class, function (Faker $faker) {
     static $password;
 
     return [
-      'customer_id' => $faker->randomDigit,
-      'wine_id' => $faker->randomDigit,
+      'customer_id' => $faker->numberBetween($min=1, $max=3),
+      'wine_id' => $faker->numberBetween($min=1, $max=10),
       'title' => $faker->word,
       'body' => $faker->text($maxNbChars = 200),
       'rating' => $faker->numberBetween($min=1, $max=5)
@@ -83,6 +83,6 @@ $factory->define(App\Order::class, function (Faker $faker) {
       'user_name' => $faker->name,
       'user_email' => $faker->unique()->safeEmail,
       'wine_name' => $faker->name,
-      'wine_id' => $faker->randomDigit,
+      'wine_id' => $faker->numberBetween($min=1, $max=10),
     ];
 });
